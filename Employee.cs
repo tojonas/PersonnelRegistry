a62c967a7f40644cdb4cef68454152ad0819382e
jonas <jonas@DESKTOP-VUQHODA>
2021-11-30 15:11:35 +0100

    Add project files.

---
+++ Employee.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace PersonnelRegistry
+{
+    public class Employee
+    {
+        public string Name { get; set; }
+        public float Salary { get; set; }
+
+        public override string ToString()
+        {
+            return string.Format("Name:{0} Salary:{1}", Name, Salary);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PersonnelRegistry
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var employees = new List<Employee>();
+            Console.WriteLine("Please enter Name and Salary separated by comma (,) exit by pressing Enter, print employees by pressing p");
+            while (true)
+            {
+                var input = Console.ReadLine();
+                switch (input)
+                {
+                    case "":
+                        return;
+
+                    case "p":
+                        PrintEmployees(employees);
+                        break;
+                    default:
+                        Employee employee = null;
+                        if (true == TryParseInput(input, out employee))
+                        {
+                            employees.Add(employee);
+                        }
+                        break;
+                }
+            }
+        }
+        private static bool TryParseInput(string input, out Employee employee)
+        {
+            employee = new Employee();
+
+            string[] parts = input.Split(",");
+            float salary = 0;
+            if (string.IsNullOrEmpty(parts[0]))
+            {
+                Console.WriteLine("Invalid name [{0}]", parts[0]);
+                return false;
+            }
+            employee.Name = parts[0];
+            if (float.TryParse(parts[1], out salary) == false || salary < 0)
+            {
+                Console.WriteLine("Invalid salary [{0}]", parts[1]);
+                return false;
+            }
+            employee.Salary = salary;
+            return true;
+        }
+
+        private static void PrintEmployees(IEnumerable<Employee> list)
+        {
+            foreach (var employee in list)
+            {
+                Console.WriteLine(employee);
+            }
+        }
+
+    }
+}
